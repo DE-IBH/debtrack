8bda4d9ebbc27aebf31646021abbc0f16bef37b5
Thomas Liske <liske@ibh.de>
2016-03-10 14:45:24 +0100

    Add test -x to cron job.

--- debian/cron.d
+++ debian/cron.d
@@ -1,4 +1,4 @@
 #
 # Regular cron jobs for the debtrack package
 #
-42 2 * * *	root	/usr/lib/debtrack/debupdatecheck.sh
+42 2 * * *	root	text -x /usr/lib/debtrack/debupdatecheck.sh && /usr/lib/debtrack/debupdatecheck.sh
